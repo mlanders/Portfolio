2942ce007a964ff6f4707359f6a529e092f657b5
Mike Landers <landers.mike@gmail.com>
2019-07-08 14:34:59 -0700

    adds working contact form and updated styling updates luncher image

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const Contact = () => {
     const [state, setState] = useState({ name: '', email: '', message: '' });
@@ -29,44 +30,71 @@ const Contact = () => {
         setState({ ...state, [e.target.name]: e.target.value });
 
     return (
-        <form name="contact" onSubmit={handleSubmit}>
-            <p>
-                <label>
-                    Your Name:{' '}
+        <Styles>
+            <form name="contact" onSubmit={handleSubmit}>
+                <p>
                     <input
                         type="text"
                         name="name"
+                        placeholder="Your name"
                         value={state.name}
                         onChange={handleChange}
                     />
-                </label>
-            </p>
-            <p>
-                <label>
-                    Your Email:{' '}
+                </p>
+                <p>
                     <input
                         type="email"
                         name="email"
+                        required
+                        placeholder="Your email address"
                         value={state.email}
                         onChange={handleChange}
                     />
-                </label>
-            </p>
-            <p>
-                <label>
-                    Message:{' '}
+                </p>
+                <p>
                     <textarea
                         name="message"
+                        placeholder="Message"
                         value={state.message}
                         onChange={handleChange}
                     />
-                </label>
-            </p>
-            <p>
-                <button type="submit">Send</button>
-            </p>
-        </form>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
+        </Styles>
     );
 };
 
 export default Contact;
+
+const Styles = styled.div`
+    input {
+        border-radius: 4px;
+        padding: 5px;
+        margin: 5px;
+        border: none;
+        width: 200px;
+    }
+    textarea {
+        border-radius: 4px;
+        padding: 5px;
+        margin: 5px;
+        border: none;
+        width: 200px;
+    }
+    button {
+        border-radius: 4px;
+        padding: 5px;
+        margin: 5px;
+        border: 1px solid #444;
+        width: 200px;
+        color: #fff;
+        background-color: #1356aa;
+        cursor: pointer;
+        &:hover {
+            background-color: #2f6dc4;
+        }
+    }
+`;
