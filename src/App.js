4918f1f4ce27b9477e6f5fb7a62e99af041d27e7
Mike Landers <landers.mike@gmail.com>
2020-03-03 19:30:25 -0800

    removed google analytics

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // Project data
-import db from './data.js';
+import db from "./data.js";
 
 // Components
-import Projects from './components/Projects/';
-import Header from './components/Header/';
-import Footer from './components/Footer/';
-import About from './components/About/';
+import Projects from "./components/Projects/";
+import Header from "./components/Header/";
+import Footer from "./components/Footer/";
+import About from "./components/About/";
 
 // Styles
-import { GlobalStyles } from './styles';
-
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-1771173-5');
-ReactGA.pageview(window.location.pathname + window.location.search);
+import { GlobalStyles } from "./styles";
 
 function App() {
-    const [data, setData] = useState([]);
+	const [data, setData] = useState([]);
 
-    useEffect(() => {
-        setData(db);
-    });
+	useEffect(() => {
+		setData(db);
+	});
 
-    return (
-        <>
-            <GlobalStyles />
-            <Header />
-            <About />
-            <Projects data={data} />
-            <Footer />
-        </>
-    );
+	return (
+		<>
+			<GlobalStyles />
+			<Header />
+			<About />
+			<Projects data={data} />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,75 +1,53 @@
-import trainingBot from './img/trainingBot.png';
-// import luncher from './img/Luncher.png';
-import lambdaNotes from './img/lambdaNotes.png';
-import landers from './img/Landers.png';
+import trainingBot from "./img/trainingBot.png";
+import lambdaNotes from "./img/lambdaNotes.png";
+import landers from "./img/Landers.png";
 
 const projects = [
-    {
-        id: 1,
-        name: 'Training Bot',
-        description:
-            'A learning application that allows a team leader to send out snippets of training information using SMS and email. Team leaders build out the messages and assign team members to receive the messages.',
-        img: `${trainingBot}`,
-        github: 'https://github.com/training-bot',
-        preview: 'https://www.trainingbot.co/',
-        tags: [
-            'fab fa-html5',
-            'fab fa-css3-alt',
-            'fab fa-js-square',
-            'fab fa-react',
-            'fab fa-node-js',
-            'fab fa-aws',
-            'fab fa-stripe'
-        ]
-    },
-    {
-        id: 2,
-        name: 'Lambda Notes',
-        description:
-            'Built in 48hrs with 3 other developers for a hackathon. Lambda Notes is an application that allows Lambda School students to take notes as they go through the cirriculum',
-        img: `${lambdaNotes}`,
-        github: 'https://github.com/orgs/lambda-notes/dashboard',
-        preview: 'https://lambda-notes-hackathon.netlify.com',
-        tags: [
-            'fab fa-html5',
-            'fab fa-css3-alt',
-            'fab fa-js-square',
-            'fab fa-node-js',
-            'fab fa-react',
-            'fab fa-aws'
-        ]
-    },
-    {
-        id: 3,
-        name: 'Landers.dev',
-        description:
-            'Portfolio site for Mike Landers. Built using React and Styled Components. Hosted on Netlify.',
-        img: `${landers}`,
-        github: 'https://github.com/mlanders/Portfolio',
-        preview: 'https://www.landers.dev/',
-        tags: [
-            'fab fa-html5',
-            'fab fa-css3-alt',
-            'fab fa-js-square',
-            'fab fa-react'
-        ]
-    }
-    // {
-    //     id: 4,
-    //     name: 'Luncher',
-    //     description:
-    //         'Early front end project built at Lambda School. Luncher is an application that allows school administrators to request donations.',
-    //     img: `${luncher}`,
-    //     github: 'https://github.com/luncher2-build-week-org-19/FE-Luncher2',
-    //     preview: 'https://luncher2.netlify.com/',
-    //     tags: [
-    //         'fab fa-html5',
-    //         'fab fa-css3-alt',
-    //         'fab fa-js-square',
-    //         'fab fa-node-js',
-    //         'fab fa-react'
-    //     ]
-    // }
+	{
+		id: 1,
+		name: "Training Bot",
+		description:
+			"A learning application that allows a team leader to send out snippets of training information using SMS and email. Team leaders build out the messages and assign team members to receive the messages.",
+		img: `${trainingBot}`,
+		github: "https://github.com/training-bot",
+		preview: "https://www.trainingbot.co/",
+		tags: [
+			"fab fa-html5",
+			"fab fa-css3-alt",
+			"fab fa-js-square",
+			"fab fa-react",
+			"fab fa-node-js",
+			"fab fa-aws",
+			"fab fa-stripe",
+		],
+	},
+	{
+		id: 2,
+		name: "Lambda Notes",
+		description:
+			"Built in 48hrs with 3 other developers for a hackathon. Lambda Notes is an application that allows Lambda School students to take notes as they go through the cirriculum",
+		img: `${lambdaNotes}`,
+		github: "https://github.com/orgs/lambda-notes/dashboard",
+		preview: "https://lambda-notes-hackathon.netlify.com",
+		tags: [
+			"fab fa-html5",
+			"fab fa-css3-alt",
+			"fab fa-js-square",
+			"fab fa-node-js",
+			"fab fa-react",
+			"fab fa-aws",
+		],
+	},
+	{
+		id: 3,
+		name: "Landers.dev",
+		description:
+			"Portfolio site for Mike Landers. Built using React and Styled Components. Hosted on Netlify.",
+		img: `${landers}`,
+		github: "https://github.com/mlanders/Portfolio",
+		preview: "https://www.landers.dev/",
+		tags: ["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-react"],
+	},
 ];
 /* 
 fab fa-react
